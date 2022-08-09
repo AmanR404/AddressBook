20b927e9664d511e20c2577bf255c49d251c4e98
Aman <raiaman21@gmail.com>
2022-08-09 19:07:38 +0530

    UC2 Solved

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -47,56 +47,27 @@ public class AddressBookMain {
 			System.out.println(contact[i]);
 		}
 	}
-	void editContacts(Scanner scanner , Contacts[] contact, String email) 
+	void deleteContacts(Scanner scanner , Contacts[] contact, String email) 
 	{
-		
-		String firstName;
-		String lastName;
-		String address;
-		String city;
-		int zip;
-		String phoneNumber;
-		
 		for (int i=0; i<contact.length;i++ ) {
 			
-			if (contact[i].getEmail().equalsIgnoreCase(email)) {
-			
-			System.out.println("Enter First name: ");
-			firstName = scanner.next();
-			contact[i].setFirstName(firstName);
-			
-			System.out.println("Enter Last name: ");
-			lastName = scanner.next();
-			contact[i].setLirstName(lastName);
-			
-			System.out.println("Enter your Address: ");
-			address = scanner.next();
-			contact[i].setAddress(address);
-			
-			System.out.println("Enter Your City: ");
-			city = scanner.next();
-			contact[i].setCity(city);
-			
-			System.out.println("Enter Zip code: ");
-			zip = scanner.nextInt();
-			contact[i].setZip(zip);
-
-			System.out.println("Enter Your Email: ");
-			email = scanner.next();
-			contact[i].setEmail(email);
-
-			System.out.println("Enter Your Number: ");
-			phoneNumber = scanner.next();
-			contact[i].setPhoneNumber(phoneNumber);
+			if(contact[i].getEmail().equalsIgnoreCase(email)) {
+			contact[i].setFirstName(null);
+			contact[i].setLirstName(null);
+			contact[i].setAddress(null);
+			contact[i].setCity(null);
+			contact[i].setZip(null);
+			contact[i].setEmail(null);
+			contact[i].setPhoneNumber(null);
 			}
 		}
 		
 		for (int i = 0 ; i<contact.length; i++) 
 		{
 			System.out.println(contact[i]);
-		}
-		
+		}	
 	}
+	
     public static void main(String[] args) {
         AddressBookMain addressbook = new AddressBookMain();
 		
@@ -108,6 +79,6 @@ public class AddressBookMain {
 		
         System.out.print("Enter a person email to edit the contact: ");
 		String email = scanner.next();
-		addressbook.editContacts(scanner, contact, email);
+		addressbook.deleteContacts(scanner, contact, email);
 		}
 }
